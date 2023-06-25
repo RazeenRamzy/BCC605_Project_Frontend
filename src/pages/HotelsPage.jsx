d8a9127e3d58748922d43f5c53fb34b1c49c614b
RazeenRamzy <razeenramzy555@gmail.com>
2023-06-25 19:25:25 +0530

    Search function in book page updated

--- src/pages/HotelsPage.jsx
+++ src/pages/HotelsPage.jsx
@@ -8,7 +8,21 @@ import imagesecond from '../asset/images/Slide2.jpeg';
 import imagethird from '../asset/images/Slide3.jpeg';
 import '../asset/css/custom-styles.css';
 
-function BookSearch() {
+function BookSearch({ searchBooks, query, setQuery, books }) {
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Enter book title or keywords"
+      />
+      <button onClick={searchBooks}>Search</button>
+    </div>
+  );
+}
+
+function HotelsPage() {
   const [query, setQuery] = useState('');
   const [books, setBooks] = useState([]);
 
@@ -25,41 +39,6 @@ function BookSearch() {
   };
 
   return (
-    <div className="container">
-      <div className="search-bar">
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Enter book title or keywords"
-        />
-        <button onClick={searchBooks}>Search</button>
-      </div>
-
-      <div className="content-grid">
-        <div className="book-column">
-          {books.map((book, index) => (
-            <div className="book-card" key={book.id}>
-              <img
-                src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail}
-                alt={book.volumeInfo.title}
-              />
-              <div className="book-details">
-                <h3>{book.volumeInfo.title}</h3>
-                <p>{book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}</p>
-                <p>{book.volumeInfo.publishedDate}</p>
-              </div>
-              {(index + 1) % 5 === 0 && <div className="column-divider" />}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function HotelsPage() {
-  return (
     <React.Fragment>
       <Menu />
       <Carousel>
@@ -67,27 +46,36 @@ function HotelsPage() {
           <img className="d-block w-100" src={imagefirst} alt="Slide1" style={{ height: '700px' }} />
           <Carousel.Caption>
             <h3 style={{ color: 'white' }}>Reading is the key that unlocks the doors of imagination, and the BCAS MLibrarian holds the treasure trove.</h3>
+            <BookSearch
+              searchBooks={searchBooks}
+              query={query}
+              setQuery={setQuery}
+              books={books}
+            />
           </Carousel.Caption>
         </Carousel.Item>
-        {/* <Carousel.Item>
-          <img className="d-block w-100" src={imagesecond} alt="Slide2" style={{ height: '700px' }} />
-          <Carousel.Caption>
-            <h3 style={{ color: 'white' }}>Reading is a passport to endless adventures, and the MLibrarian is your trusted guide in this remarkable journey.</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={imagethird} alt="Slide3" style={{ height: '700px' }} />
-          <Carousel.Caption>
-            <h3 style={{ color: 'white' }}>Reading with the aid of MLibrarian, where technology meets literature, creates a harmonious symphony of learning and innovation.</h3>
-          </Carousel.Caption>
-        </Carousel.Item> */}
       </Carousel>
       <header className="page-header">
         <h1>Welcome to Hotels Page</h1>
       </header>
       <Container>
         <Row>
-          <BookSearch />
+          <div className="book-column">
+            {books.map((book, index) => (
+              <div className="book-card" key={book.id}>
+                <img
+                  src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail}
+                  alt={book.volumeInfo.title}
+                />
+                <div className="book-details">
+                  <h3>{book.volumeInfo.title}</h3>
+                  <p>{book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}</p>
+                  <p>{book.volumeInfo.publishedDate}</p>
+                </div>
+                {(index + 1) % 5 === 0 && <div className="column-divider" />}
+              </div>
+            ))}
+          </div>
         </Row>
       </Container>
       <Footer />
