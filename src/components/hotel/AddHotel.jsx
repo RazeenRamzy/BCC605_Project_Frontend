a991888ae0bf348e21f95286ba2dc6210dfcfc5b
RazeenRamzy <razeenramzy555@gmail.com>
2023-06-25 14:25:56 +0530

    update

--- src/components/hotel/AddHotel.jsx
+++ src/components/hotel/AddHotel.jsx
@@ -1,13 +1,25 @@
 import React, { Component, Fragment } from 'react'
+import Menu from '../components/menu/Menu'
+import Footer from '../components/footer/Footer'
+
 
 export class AddHotel extends Component {
   render() {
     return (
       <Fragment>
         
-      </Fragment>
-    )
-  }
+        <Menu />
+
+          <div style={{ marginTop: '70px', position: 'relative' }}>
+          <HotelsPage />
+          </div>
+          <Footer />
+          </Fragment>
+)
 }
+}
+        
+     
+
 
 export default AddHotel
