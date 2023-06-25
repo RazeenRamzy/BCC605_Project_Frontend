2871c6e016bdf5095e2625e1fa240c26b245ed6d
RazeenRamzy <razeenramzy555@gmail.com>
2023-06-25 19:54:04 +0530

    FAQ page updated

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -47,7 +47,39 @@ class Faq extends Component {
                 </p>
               </MDBCol>
 
-              {/* Add more FAQ items as needed */}
+              <MDBCol md="6" lg="4" className="mb-4">
+                <MDBTypography tag="h6" className="mb-3 text-primary">
+                  <MDBIcon fas icon="pen-alt text-primary pe-2" /> How can I borrow books from MLibrarian?
+                </MDBTypography>
+                <p>
+                  Borrowing books from MLibrarian is easy. Simply search for the book you want, check its availability,
+                  and click on the "Borrow" button. You can then pick up the book from the designated library location.
+                  Make sure to return the book within the specified due date to avoid any late fees.
+                </p>
+              </MDBCol>
+
+              <MDBCol md="6" lg="4" className="mb-4">
+                <MDBTypography tag="h6" className="mb-3 text-primary">
+                  <MDBIcon fas icon="pen-alt text-primary pe-2" /> How does MLibrarian handle book recommendations?
+                </MDBTypography>
+                <p>
+                  MLibrarian uses a collaborative filtering algorithm to generate personalized book recommendations.
+                  It analyzes the reading history and preferences of other students with similar interests to suggest
+                  books that you may find interesting. The more you interact with the system, the better the recommendations
+                  become as it learns your preferences over time.
+                </p>
+              </MDBCol>
+
+              <MDBCol md="6" lg="4" className="mb-4">
+                <MDBTypography tag="h6" className="mb-3 text-primary">
+                  <MDBIcon fas icon="pen-alt text-primary pe-2" /> Can I suggest books to be added to MLibrarian?
+                </MDBTypography>
+                <p>
+                  Absolutely! We welcome book suggestions from BCAS students. If there's a specific book you would like to see
+                  added to the MLibrarian collection, please contact us through the "Suggest a Book" feature in the website or portal.
+                  Our team will review the suggestion and do their best to acquire the book for the library.
+                </p>
+              </MDBCol>
 
             </MDBRow>
 
