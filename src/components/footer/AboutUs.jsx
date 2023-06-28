993dacfa2028cc7a34c1ed7e9a642247ee1bf6bf
RazeenRamzy <razeenramzy555@gmail.com>
2023-06-28 09:17:09 +0530

    MLibrarian About us page

--- src/components/footer/AboutUs.jsx
+++ src/components/footer/AboutUs.jsx
@@ -1,52 +1,75 @@
-import React, { Component } from 'react';
+import React from 'react';
+import MLibrarian from '../../asset/images/MLibrarian.png';
 
-class AboutUs extends Component {
-  render() {
-    return (
-      <div className="about-us-page">
-        <div className="banner-section">
-          <h1>Welcome to BCAS MLibrarian</h1>
-          <p>Your Gateway to Knowledge and Learning</p>
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <div className="about-us-content">
+        <h1 className="about-us-title2" >Welcome to BCAS MLibrarian</h1>
+        <p className="about-us-description">Your Gateway to Knowledge</p>
+        <div className="about-us-image-container">
+          <img
+            src={MLibrarian}
+            alt="Library"
+            className="about-us-image"
+          />
         </div>
 
-        <div className="content-section">
-          <div className="mission-section">
-            <h2>Our Mission</h2>
-            <p>
-              At BCAS MLibrarian, our mission is to empower BCAS students with easy access to a wide range of educational resources and promote a culture of reading and continuous learning.
-            </p>
-          </div>
+        <h3></h3>
+        <p className="about-us-text">
+          At BCAS MLibrarian, we believe in the power of knowledge and the importance of books in shaping the minds of BCAS
+          students. Our mission is to provide a comprehensive and accessible library experience, tailored to meet the
+          needs of BCAS students on their educational journey.
+        </p>
+        <p className="about-us-text">
+          Our collection consists of a diverse range of books, covering various subjects and disciplines, allowing students
+          to expand their horizons and deepen their understanding of their chosen fields. From textbooks to research
+          materials, we strive to provide the resources necessary to excel academically.
+        </p>
+        <p className="about-us-text">
+          To enhance the learning experience, we have integrated cutting-edge technology and innovative features into our
+          library. Explore our digital collection, engage in online discussions, and take advantage of personalized
+          recommendations tailored to your interests. With our user-friendly interface and intuitive search capabilities,
+          finding the right resources has never been easier.
+        </p>
+        <p className="about-us-text">
+          We are committed to supporting BCAS students throughout their educational journey by fostering a culture of
+          reading, research, and intellectual growth. Join us in discovering the joy of reading and unlocking the vast
+          possibilities that lie within the pages of a book.
+        </p>
 
-          <div className="features-section">
-            <h2>Key Features</h2>
-            <ul>
-              <li>Extensive collection of books and study materials</li>
-              <li>24/7 online access to resources</li>
-              <li>Personalized recommendations based on your interests</li>
-              <li>Collaborative learning opportunities with fellow BCAS students</li>
-              <li>Access to exclusive educational events and workshops</li>
-            </ul>
-          </div>
+        <h3 className="about-us-title">Our Mission</h3>
+        <p className="about-us-text">
+          At BCAS MLibrarian, our mission is to empower BCAS students with the resources and tools they need to thrive in their educational journey. We strive to create a dynamic and inclusive learning environment that fosters intellectual growth, curiosity, and a passion for knowledge.
+        </p>
 
-          <div className="testimonial-section">
-            <h2>What BCAS Students Say</h2>
-            <div className="testimonial">
-              <blockquote>
-                "BCAS MLibrarian has been a game-changer for my studies. The vast collection of resources and personalized recommendations have helped me excel in my academic journey."
-              </blockquote>
-              <cite>- John Doe, BCAS Student</cite>
-            </div>
-            <div className="testimonial">
-              <blockquote>
-                "I appreciate the convenience of accessing books and study materials anytime, anywhere. BCAS MLibrarian has truly made learning enjoyable and accessible for BCAS students."
-              </blockquote>
-              <cite>- Jane Smith, BCAS Student</cite>
-            </div>
-          </div>
-        </div>
+        <h3 className="about-us-title">Our Collection</h3>
+        <p className="about-us-text">
+          Our library houses a vast collection of books covering a wide range of subjects and disciplines. From textbooks and reference materials to literature and research resources, we curate our collection to support the diverse interests and academic pursuits of BCAS students.
+        </p>
+
+        <h3 className="about-us-title">Innovative Technology</h3>
+        <p className="about-us-text">
+          Embracing the digital era, we have integrated innovative technology into our library to enhance the learning experience. Our online platform offers access to digital resources, interactive learning tools, and collaborative spaces, empowering students to engage with knowledge in new and exciting ways.
+        </p>
+
+        <h3 className="about-us-title">Community and Support</h3>
+        <p className="about-us-text">
+          BCAS MLibrarian is not just a collection of books; it is a vibrant community of learners. We provide a supportive environment where students can connect with peers, engage in discussions, and collaborate on projects. Our dedicated team is here to assist and guide students on their academic journey.
+        </p>
+
+        <h3 className="about-us-title">Join Us</h3>
+        <p className="about-us-text">
+          We invite all BCAS students to join us in discovering the power of books and the joy of learning. Whether you are seeking academic resources, exploring new subjects, or expanding your horizons, BCAS MLibrarian is here to accompany you on your quest for knowledge.
+        </p>
+
+
+        
       </div>
-    );
-  }
-}
+
+      
+    </div>
+  );
+};
 
 export default AboutUs;
